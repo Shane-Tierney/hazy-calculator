a28db747cb8d54d7dafbd784f0ac76448651632d
Shane Tierney <sptierney713@gmail.com>
2022-04-23 17:39:18 -0400

    adds arrayFilter function to index.js

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const calculate = (array) => {
-    
+
 }
 
 const arrayAssign = (array) => {
-    let numOne = null
-    let numTwo = null
-    let operator
-    const opArray = ['*', '/', '-', '+']
+  let numOne = null
+  let numTwo = null
+  let operator
+  const opArray = ['*', '/', '-', '+']
 
-    for (let i = 0; i < array.length; i++) {
-        const item = array[i]
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i]
 
-        if(Number(item) && numOne === null) {
-            numOne = item
-        } else if (Number(item) && numOne !== null) {
-            numTwo = item
-        } else if (opArray.includes(item)) {
-            operator = item
-        }
+    if (Number(item) && numOne === null) {
+      numOne = item
+    } else if (Number(item) && numOne !== null) {
+      numTwo = item
+    } else if (opArray.includes(item)) {
+      operator = item
     }
+  }
 }
 
-const 
+const arrayFilter = (array) => {
+  let filteredArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i]
+
+    if (item) {
+      filteredArray.push(item)
+    }
+  }
+
+  return filteredArray
+}
